78e08fdee00bca7a7935428606a6f4dd0345d12f
Mustafa Gönültaş <73774639+gonultasmf@users.noreply.github.com>
2022-09-05 21:33:05 +0300

    Add files via upload

---
+++ BMI/BMI/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BMI.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public Models.BMI BMI { get; set; }
+        public BMIViewModel()
+        {
+            BMI = new Models.BMI();
+            BMI.Height = 180;
+            BMI.Weight = 73;
+        }
+    }
+}
